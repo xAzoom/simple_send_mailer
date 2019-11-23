cbfe5312084b0c8e6d2fc3d723e8b9de7f9b4ae9
Patryk Wilusz <patryk.wilusz@brandmed.pl>
2019-11-23 21:11:29 +0100

    Add swift mailer

--- src/Controller/MailerController.php
+++ src/Controller/MailerController.php
@@ -12,8 +12,19 @@ class MailerController extends AbstractController
     /**
      * @Route("/send_email", methods={"GET"}, name="api_send_email")
      */
-    public function sendMail(): Response
+    public function sendMail(\Swift_Mailer $swiftMailer, string $mail): Response
     {
-        return new JsonResponse(['message' => 'package']);
+//        $swiftMessage = (new \Swift_Message('Mam temacik dla Ciebie'))
+//            ->setTo('morenequs@gmail.com', 'Patryk')
+//            ->setFrom('azoomofficial@gmail.com', 'Polska Husaria')
+//            ->setBody(
+//                'No siemanko mordeczko<br>Może chciałbyś co nie co hehehe',
+//                'text/html'
+//            )
+//        ;
+//
+//        $swiftMailer->send($swiftMessage);
+
+        return new JsonResponse(['message' => $mail]);
     }
 }
