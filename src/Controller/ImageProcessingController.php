1ace9344da6d31832f53c601a225beaeb731600b
Patryk Wilusz <patryk.wilusz@brandmed.pl>
2019-11-24 01:22:58 +0100

    Add checking image type before scaling

--- src/Controller/ImageProcessingController.php
+++ src/Controller/ImageProcessingController.php
@@ -3,23 +3,36 @@
 namespace App\Controller;
 
 use Gumlet\ImageResize;
+use Psr\Log\LoggerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\BinaryFileResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\ResponseHeaderBag;
+use Symfony\Component\HttpKernel\Exception\HttpException;
 use Symfony\Component\Routing\Annotation\Route;
 
 class ImageProcessingController extends AbstractController
 {
+    const PNG = 'PNG';
+    const JPEG = 'JPEG';
+
+    const MAP = [
+        IMAGETYPE_PNG => self::PNG,
+        IMAGETYPE_JPEG => self::JPEG,
+    ];
+
     /**
      * @Route("/scale", name="api_scale_image")
      */
-    public function resize(Request $request): Response
+    public function resize(Request $request, LoggerInterface $imagesLogger, array $availableImages): Response
     {
         $scale = $request->query->get('scale') ?:100;
-
         $image = ImageResize::createFromString($request->getContent());
+
+        if (!isset(self::MAP[$image->source_type]) || !in_array(self::MAP[$image->source_type], $availableImages)) {
+            throw new HttpException(400, 'Unsupported image type');
+        }
+
         $image->scale($scale);
 
         $response = new Response();
@@ -30,6 +43,4 @@ class ImageProcessingController extends AbstractController
 
         return $response;
     }
-
-
 }
